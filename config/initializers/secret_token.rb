808986e7d711cd727e2df771f411733308cde2ca
Tom Scott <tubbo@psychedeli.ca>
2012-04-23 19:17:46 -0400

    initial commit of thewonderbars.com

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bread::Application.config.secret_token = '87caba470f35b84828ac8157828b32b50e084fb1f5f07daafb589cadcab913ac0c254eb4f29edb69cf7cba5843627c9de25eb19c7014b070303ee6934e332e76'
