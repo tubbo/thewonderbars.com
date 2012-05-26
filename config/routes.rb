aefdb92a972b05b118031e358c4d751c5ca7a8b7
Tom Scott <tubbo@psychedeli.ca>
2012-05-25 23:41:54 -0400

    Added real shows page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Bread::Application.routes.draw do
   resources :releases
 
-  get "/shows" => 'facebook#events'
+  get "/shows" => 'facebook#shows'
   get "/members" => 'facebook#band_members'
   get "/about" => 'facebook#about_us'
   get "/contact" => 'facebook#contact'
