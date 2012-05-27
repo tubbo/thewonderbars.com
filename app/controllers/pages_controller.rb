9accdebcf9eb2d3b8a4ea7c928ffc5e0a101d217
Tom Scott <tubbo@psychedeli.ca>
2012-05-27 13:59:28 -0400

    fixed bs error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 # Includes static content exclusive to this site. If we need it.
 class PagesController < ApplicationController
   def index
+    render layout: false if request.xhr?
+  end
+
+  def shows
+    render layout: false if request.xhr?
   end
 end
