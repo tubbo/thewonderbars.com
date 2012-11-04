c86bcaa180963f8ee6d621c56ec3b022822fbc4c
Tom Scott <tubbo@psychedeli.ca>
2012-11-04 13:01:56 -0500

    Remigrate and fix CSS issues on modern browsers

--- db/schema.rb
+++ db/schema.rb
@@ -24,12 +24,4 @@ ActiveRecord::Schema.define(:version => 20120610184352) do
     t.date     "released_on"
   end
 
-  create_table "spatial_ref_sys", :id => false, :force => true do |t|
-    t.integer "srid",                      :null => false
-    t.string  "auth_name", :limit => 256
-    t.integer "auth_srid"
-    t.string  "srtext",    :limit => 2048
-    t.string  "proj4text", :limit => 2048
-  end
-
 end
