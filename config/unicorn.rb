84b79afea38f0cfc1d06833a34a13aac1e7325ee
Tom Scott <tubbo@psychedeli.ca>
2013-06-10 10:19:24 -0400

    Fix Unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,52 +1,24 @@
-## Unicorn configuration
+# Configured for use on Heroku
 
-# Store the PID in the Capistrano app dir
-pid_file = "/home/necromancer/src/wonderbars/shared/pids/unicorn.pid"
+worker_processes ENV["WEB_CONCURRENCY"].to_i || 4
+timeout 15
+preload_app true
 
-# What ports/sockets to listen on, and what options for them.
-listen 7000, backlog: 64
-
-working_directory "/home/necromancer/src/wonderbars/current"
-
-# What the timeout for killing busy workers is, in seconds
-timeout 60
-
-# Whether the app should be pre-loaded
-preload_app false
-
-# How many worker processes
-worker_processes 4
-
-# What to do before we fork a worker
 before_fork do |server, worker|
-  # the following is highly recomended for Rails + "preload_app true"
-  # as there's no need for the master process to hold a connection
-  if defined?(ActiveRecord::Base)
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
-  end
-
-  # Before forking, kill the master process that belongs to the .oldbin PID.
-  # This enables 0 downtime deploys.
-  old_pid = "#{pid_file}.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
 end
 
-# What to do after we fork a worker
 after_fork do |server, worker|
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
 end
-
-# Where to drop a pidfile
-pid pid_file
-
-# Where stderr gets logged
-stderr_path "/home/necromancer/src/wonderbars/shared/log/error.log"
-
-# Where stdout gets logged
-stdout_path "/home/necromancer/src/wonderbars/shared/log/access.log"
