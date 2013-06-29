4c0d0996c15885635d290f1afa00d1fe2f45368f
Tom Scott <tubbo@psychedeli.ca>
2013-06-29 18:04:19 -0400

    Add features and move social partials

---
+++ spec/features/view_shows_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+feature "Static pages" do
+
+  context "for the shows listing" do
+    before { visit '/shows' }
+
+    it "renders a title and some greeting text" do
+      expect(page).to have_content "Wanna come see us play?"
+    end
+
+    it "renders the upcoming shows table"
+    it "renders the past shows table"
+  end
+
+  context "for the contact page" do
+    before { visit '/contact' }
+
+    it "renders the static content" do
+      expect(page).to have_content 'Contact The Wonder Bars'
+    end
+  end
+end
---
+++ spec/features/visit_homepage_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+feature "Visiting the home page" do
+  before { visit '/' }
+
+  it "shows an empty desktop" do
+    expect(page).to have_element '#desktop'
+  end
+end
