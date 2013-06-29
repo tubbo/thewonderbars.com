c4a8f89afafd6ff264c5679d3b57233426a483f3
Tom Scott <tubbo@psychedeli.ca>
2013-06-29 18:22:48 -0400

    Apply decorators to returned releases_controller content

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -4,13 +4,13 @@ class ReleasesController < ApplicationController
 
   # Return all releases
   def index
-    @releases = []
+    @releases = Release.all.decorate_collection
     render layout: false if request.xhr?
   end
 
   # Return a single release by ID
   def show
-    @release = Release.find params[:id]
+    @release = Release.find(params[:id]).decorate
     render layout: false if request.xhr?
   end
 end
