5e20d425e99f8da8e07098a1f99cc14f9de2d20d
Tom Scott <tubbo@psychedeli.ca>
2013-06-29 11:52:54 -0400

    Fix syntax on tests

--- test/functional/facebook_controller_test.rb
+++ test/functional/facebook_controller_test.rb
@@ -2,20 +2,20 @@ require 'test_helper'
 
 class FacebookControllerTest < ActionController::TestCase
   setup do
-    VCR.use_cassette(:facebook_authentication) do
+    VCR.use_cassette :facebook_authentication do
       @controller = FacebookController.new
     end
   end
 
   test "access the bio page" do
-    VCR.use_cassette(:facebook_authentication) do
+    VCR.use_cassette :facebook_authentication do
       get :about_us
       assert response.success?
     end
   end
 
   test "access the contact page" do
-    VCR.use_cassette(:facebook_authentication) do
+    VCR.use_cassette :facebook_authentication do
       get :contact
       assert response.success?
     end
